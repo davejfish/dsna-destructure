5cd3290c28998c59bdd200e6a04beb4ea807901d
Dave Fisher <dave.j.fish@gmail.com>
2022-09-13 11:12:15 -0700

    syntax.

--- index.js
+++ index.js
@@ -19,14 +19,20 @@ export const printAddress = ({ name, address }) => {
 // REFACTOR CHALLENGE
 // Refactor this function so that all values in the object are destructured
 // as part of the funciton definitions (i.e. there should be no dots in the template literals)
-export const printUserInfo = (user) => {
+export const printUserInfo = ({ 
+  username, 
+  name: {first, last}, 
+  info: {favorites: {food, color}},
+  info: {pet: {type, name}},
+  info: {address: {street, number, city, country}}
+}) => {
   return `
-    Username: ${user.username},
-    Full Name: ${user.name.first} ${user.name.last},
-    Favorite Color: ${user.info.favorites.color},
-    Favorite Food: ${user.info.favorites.food},
-    Pet Name: ${user.info.pet.name},
-    Address: ${user.info.address.number} ${user.info.address.street}, ${user.info.address.city}, ${user.info.address.country}
+    Username: ${username},
+    Full Name: ${first} ${last},
+    Favorite Color: ${color},
+    Favorite Food: ${food},
+    Pet Name: ${name},
+    Address: ${number} ${street}, ${city}, ${country}
     `
 }
 
