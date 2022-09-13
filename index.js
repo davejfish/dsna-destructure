bf1fc741c4f98c75bb0d9d7502a74cb0ba336762
Dave Fisher <dave.j.fish@gmail.com>
2022-09-13 11:22:42 -0700

    4th test passing

--- index.js
+++ index.js
@@ -41,7 +41,11 @@ export const printUserInfo = ({
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {}
+export const getSum = (...rest) => {
+  let result = 0;
+  rest.forEach(num => result += num)
+  return result;
+}
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
